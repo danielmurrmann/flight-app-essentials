4abb53ff49600fd475c1628d63e2853eaf30dbca
Daniel Murrmann <daniel.murrmann@fancy-development.net>
2025-10-08 15:40:47 +0200

    602 - Add an Event Binding to the FlightCard Component

--- src/app/flight-card/flight-card.ts
+++ src/app/flight-card/flight-card.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { initialFlight } from '../entities/flight';
 import { DatePipe, NgStyle } from '@angular/common';
 import { CityPipe } from '../util/city-pipe';
@@ -11,12 +11,15 @@ import { CityPipe } from '../util/city-pipe';
 export class FlightCard {
   @Input() item = initialFlight;
   @Input() selected = false;
+  @Output() selectedChange = new EventEmitter<boolean>();
 
   select() {
     this.selected = true;
+    this.selectedChange.emit(this.selected);
   }
 
   deselect() {
     this.selected = false;
+    this.selectedChange.emit(this.selected);
   }
 }
