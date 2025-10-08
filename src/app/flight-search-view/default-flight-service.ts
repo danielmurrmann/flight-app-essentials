3bf2927ee42951fd5fba232b04037a8b0dd211cd
Daniel Murrmann <daniel.murrmann@fancy-development.net>
2025-10-08 10:30:21 +0200

    305 - Create an Injection Token for the Base URL

--- src/app/flight-search-view/default-flight-service.ts
+++ src/app/flight-search-view/default-flight-service.ts
@@ -3,12 +3,14 @@ import { Observable } from 'rxjs';
 import { Flight } from '../entities/flight';
 import { HttpClient } from '@angular/common/http';
 import { FlightService } from './flight-service';
+import { BASE_URL } from '../util/base-url';
 
 export class DefaultFlightService implements FlightService {
   private httpClient = inject(HttpClient);
+  private baseUrl = inject(BASE_URL);
 
   loadFlights(from: string, to: string): Observable<Flight[]> {
-    const url = 'http://demo.angulararchitects.io/api/flight';
+    const url = this.baseUrl + 'flight';
     const params = { from, to };
     const headers = { Accept: 'application/json' };
     return this.httpClient.get<Flight[]>(url, { params, headers });
